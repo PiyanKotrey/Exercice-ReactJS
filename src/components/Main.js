a26cd69d580c694854901f5b197a487f3c14c25e
Piyan.Kotrey <korttry72@gmail.com>
2023-04-17 22:19:22 +0700

    :confirm

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>
+        <div className='product'>
+            <div class="container">
+		        <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4 no-padding">
+		            <img src="http://theshop-cambodia.com/wp-content/uploads/2016/10/Food-Drink-Cambodia-1.jpg" alt="img1" />
+	            </div>
+                <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4 no-padding">
+                <img src="http://theshop-cambodia.com/wp-content/uploads/2016/10/Food-Drink-Cambodia-2.jpg" alt="img1" class="img-responsive"/>
+	            </div>
+                <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4 no-padding">
+                <img src="http://theshop-cambodia.com/wp-content/uploads/2016/10/Food-Drink-Cambodia-4.jpg" alt="img1" class="img-responsive"/>
+	            </div>
+            </div>
+        </div>
+        <div className='feedback'>
+            <div class="content-page">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3><strong>We cater for all events!</strong></h3>
+            
+            <p >Catering is in our blood, and over the years we have built a well-earned reputation as Cambodia's best event caterer.</p>
+            
+            <p >If you're wondering what types of events we cover, we can say with confidence that we've done them all: from embassy receptions to NGO gatherings, from boat parties to weddings, black-tie functions to informal get-togethers. We can provide simple snacks and soft drinks or serve a five-course meal with wine and champagne.</p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p >If you want Asian food, we can do that. Or if you prefer Western food, we've got that covered too. And we don't limit ourselves to Phnom Penh: if you need an event catered in the north, east, south or west of Cambodia, the chances are we've been there before, and we're more than happy to go there again.</p>
+            
+            <p >Nearly two decades of catering means no event is too big or too small for us, and, with our uniformed staff trained by international management, we can guarantee consistent and reliable service for your function.&nbsp;To enquire about our catering services, please get in touch:</p>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <ul className='nav'>
+            <li>
+                <a >Home</a>
+            </li>
+            <li>
+                <a >About Us</a>
+            </li>
+            <li>
+                <a >Contact</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
